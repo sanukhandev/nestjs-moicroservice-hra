a4d7793e47c556c667295d9474be829df35ada60
Sanu Khan <sanulgbello@gmail.com>
2022-07-10 22:13:58 +0400

    Update clean up

--- hr-gate-way/src/service.registery.ts
+++ hr-gate-way/src/service.registery.ts
@@ -1,6 +1,5 @@
 import { ClientProviderOptions, Transport } from "@nestjs/microservices";
 
-export const AuthService: ClientProviderOptions = { name: 'authService', transport: Transport.TCP, options: {host:'auth-service', port: 3001 } }
 export const DBservice: ClientProviderOptions = { name: 'dbService', transport: Transport.TCP, options: {host:'db-service', port: 3002 } }
 
 
